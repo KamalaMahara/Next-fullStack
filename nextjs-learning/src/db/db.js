75244c3b161099cdf7adccee99f320b5a14e46e9
Kamala Mahara <kmlamahara@gmail.com>
2026-05-09 22:26:28 +0545

    feat: add migration script and update package.json for database migrations

--- nextjs-learning/src/db/db.js
+++ nextjs-learning/src/db/db.js
@@ -3,5 +3,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 
 const connectionString = "postgresql://postgres.pyeujbhtgquoooxgajnk:Recipe@Sharing123@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
-const connection = postres(connectionString);
+export const connection = postres(connectionString);
 export const db = drizzle(connection);
